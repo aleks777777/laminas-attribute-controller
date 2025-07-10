368f265cef5cc3417e62347649bdf2751d29fd78
aleks777777 <web.naxt@gmail.com>
2025-07-10 14:50:24 +0300

    fix target

--- src/Security/PermissionGuard.php
+++ src/Security/PermissionGuard.php
@@ -6,7 +6,7 @@ namespace LaminasAttributeController\Security;
 
 use Attribute;
 
-#[Attribute(Attribute::TARGET_PARAMETER)]
+#[Attribute(Attribute::TARGET_METHOD)]
 final readonly class PermissionGuard
 {
     public function __construct(public string $permission)
